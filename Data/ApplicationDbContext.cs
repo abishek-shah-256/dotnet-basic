b4b8c3d9fc12bcc0e02c0e8e0bdabbd7703be0d0
abishek-shah-256 <shahabishek63@gmail.com>
2023-06-12 21:12:29 +0545

    uploading crud

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Demo1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo1.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
